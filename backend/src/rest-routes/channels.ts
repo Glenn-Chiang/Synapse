a8fc408cc78cb813c35f6dc6bf613082772312f1
Glenn Chiang <glennchiang24@gmail.com>
2023-10-20 19:49:36 +0800

    In createChannel handler, added logic to automatically add channel creator as a channel member Added UserChannel join table to schema

--- backend/src/rest-routes/channels.ts
+++ backend/src/rest-routes/channels.ts
@@ -1,58 +1,78 @@
-import {Router} from 'express'
-import { prisma } from '../app'
+import { Router } from "express";
+import { prisma } from "../app";
 
-const channelsRouter = Router()
+const channelsRouter = Router();
 
 // Get all channels
-channelsRouter.get('/channels', async (req, res) => {
+channelsRouter.get("/channels", async (req, res) => {
   const channels = await prisma.channel.findMany({
     include: {
-      members: true
-    }
-  })
-  return res.json(channels)
-})
+      members: true,
+    },
+  });
+  return res.json(channels);
+});
 
 // Get user channels
-channelsRouter.get('/users/:userId/channels', async (req, res) => {
-  const userId = req.params.userId
+channelsRouter.get("/users/:userId/channels", async (req, res) => {
+  const userId = req.params.userId;
 
   const channels = await prisma.channel.findMany({
     where: {
       members: {
         some: {
-          id: userId
-        }
-      }
-    }, include: {
-      members: true
-    }
-  })
+          userId
+        },
+      },
+    },
+    include: {
+      members: true,
+    },
+  });
 
-  return res.json(channels)
-})
+  return res.json(channels);
+});
 
 // Create new channel
-channelsRouter.post('/channels', async (req, res) => {
-  const {name, about} = req.body
+channelsRouter.post("/channels", async (req, res) => {
+  const { name, about, iconUrl } = req.body;
 
-  if (!name || typeof name !== 'string' || name.length > 25) {
-    return res.status(400).json('invalid name')
+  if (!name || typeof name !== "string" || name.length > 25) {
+    return res.status(400).send("invalid name");
   }
 
-  if (!about || typeof about !== 'string' || about.length > 500) {
-    return res.status(400).json('invalid about')
+  if (!about || typeof about !== "string" || about.length > 500) {
+    return res.status(400).send("invalid about");
   }
 
-  const userId = '1' // todo
+  if (!iconUrl || typeof iconUrl !== "string") {
+    return res.status(400).send("invalid iconUrl");
+  }
+
+  const userId = "1"; // todo
 
   const channel = await prisma.channel.create({
     data: {
-      name, about, creatorId: userId
-    }
-  })
+      name,
+      about,
+      creatorId: userId,
+      iconUrl,
+      members: {
+        create: [
+          {
+            user: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
 
-  return channel
-})
+  console.log("Channel created");
+  return res.json(channel);
+});
 
-export {channelsRouter}
+export { channelsRouter };
