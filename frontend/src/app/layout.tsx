9b7e721ac3d4d72b53f1a4d15d1bccb0b06d7853
Glenn Chiang <glennchiang24@gmail.com>
2023-10-23 15:05:58 +0800

    Added logo to topbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { MessageListener } from '@/components/MessageListener';
+import { MessageListener } from "@/components/MessageListener";
 import { socket } from "@/lib/socket";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -8,11 +8,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
+import Image from "next/image";
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const revalidate = 0
+export const revalidate = 0;
 
 export const metadata: Metadata = {
   title: "Synapse",
@@ -23,13 +24,12 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  
   return (
     <html lang="en">
-      <body >
-        <MessageListener/>
+      <body>
+        <MessageListener />
         <Topbar />
-        <div className="mt-16 p-4 sm:px-10">{children}</div>
+        <div className="mt-16 p-4 py-0 sm:px-10">{children}</div>
       </body>
     </html>
   );
@@ -37,11 +37,17 @@ export default async function RootLayout({
 
 function Topbar() {
   return (
-    <nav className="z-10 bg-slate-950 w-screen h-16 shadow p-4 px-8 fixed top-0 left-0 font-medium flex items-center text-xl">
-      <div className="flex gap-2 items-center">
-        <FontAwesomeIcon icon={faBars} />
-        <Link href={'/'}>Synapse</Link>
-      </div>
+    <nav className="z-10 bg-slate-950 w-screen h-16 shadow p-4 sm:px-10 fixed top-0 left-0 font-medium flex items-center text-xl">
+      <Link href={"/"} className="flex gap-2 items-center">
+        <Image
+          src={"https://static.thenounproject.com/png/40466-200.png"}
+          alt=""
+          width={40}
+          height={40}
+          className="invert"
+        />
+        Synapse
+      </Link>
     </nav>
   );
 }
