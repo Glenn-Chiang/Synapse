e0aaec091e5291153f24feef3a9c4611526d0b04
Glenn Chiang <glennchiang24@gmail.com>
2023-10-25 16:29:24 +0800

    close actionmenu by clicking outside it

--- frontend/src/app/(main)/channel/[channelId]/ChannelHeader.tsx
+++ frontend/src/app/(main)/channel/[channelId]/ChannelHeader.tsx
@@ -5,10 +5,32 @@ import { LeaveChannelModal } from "@/components/LeaveChannelModal";
 import { ActionButton, BackButton } from "@/components/buttons";
 import { Channel } from "@/lib/types";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export const ChannelHeader = ({ channel }: { channel: Channel }) => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const hideMenu = (event: MouseEvent) => {
+    if (
+      menuRef.current &&
+      event.target instanceof Node &&
+      !menuRef.current.contains(event.target)
+    ) {
+      setMenuIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (menuIsOpen) {
+      document.addEventListener("click", hideMenu);
+    } else {
+      document.removeEventListener("click", hideMenu);
+    }
+
+    return () => document.removeEventListener("click", hideMenu);
+  }, [menuIsOpen]);
 
   return (
     <header className="fixed top-16 left-0 w-full flex justify-between p-2 items-center border-b border-slate-500 z-10 bg-slate-950 ">
@@ -24,7 +46,14 @@ export const ChannelHeader = ({ channel }: { channel: Channel }) => {
         </div>
       </Link>
       <ActionButton onClick={() => setMenuIsOpen((prev) => !prev)} />
-      {menuIsOpen && <ActionMenu />}
+      {menuIsOpen && (
+        <div
+          ref={menuRef}
+          className="absolute right-2 -bottom-8 z-20 bg-slate-900 rounded-md"
+        >
+          <ActionMenu />
+        </div>
+      )}
     </header>
   );
 };
@@ -33,7 +62,7 @@ const ActionMenu = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
-    <div className="absolute right-2 -bottom-8 z-20 bg-slate-900 rounded-md">
+    <div className="">
       <button
         onClick={() => setModalIsOpen(true)}
         className="p-2 hover:bg-slate-800 rounded-md"
