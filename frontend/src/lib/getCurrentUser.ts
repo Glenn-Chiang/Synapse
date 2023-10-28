379deff3d8ce3094030505fdac4a299d98df7110
Glenn Chiang <glennchiang24@gmail.com>
2023-10-28 10:44:54 +0800

    redirect to login instead of throwing error if no cookies are found in getCurrentUser

--- frontend/src/lib/getCurrentUser.ts
+++ frontend/src/lib/getCurrentUser.ts
@@ -1,6 +1,7 @@
 import { User } from "@/lib/types";
 import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const getCurrentUser = () => {
   const cookieStore = cookies();
@@ -9,7 +10,8 @@ const getCurrentUser = () => {
 
   // TODO: how to properly handle this?
   if (!userCookie) {
-    throw new Error("missing user cookie");
+    redirect('/login')
+    // throw new Error("missing user cookie");
   }
 
   const currentUser: User = JSON.parse(userCookie.value);
