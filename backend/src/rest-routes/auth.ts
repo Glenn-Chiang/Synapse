8ba945f1a3a3df28417be9d7bedb3513778e58c5
Glenn Chiang <glennchiang24@gmail.com>
2023-10-30 14:21:32 +0800

    Added login route using local strategy

--- backend/src/rest-routes/auth.ts
+++ backend/src/rest-routes/auth.ts
@@ -5,6 +5,28 @@ import passport from "passport";
 
 const authRouter = Router();
 
+authRouter.get(
+  "/auth/login",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    const user = req.user as User;
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    });
+    res.cookie("user", JSON.stringify(user), {
+      httpOnly: false,
+      sameSite: "none",
+      secure: true,
+    });
+
+    res.json({ user, token });
+  }
+);
+
 // When user clicks 'sign in with google' in client app, a request is sent to this endpoint which redirects to the google sign-in page
 authRouter.get(
   "/auth/google",
@@ -35,7 +57,7 @@ authRouter.get(
       sameSite: "none",
       secure: true,
     });
-    res.redirect(process.env.CLIENT_URL as string)
+    res.redirect(process.env.CLIENT_URL as string);
     console.log(`${user.username} has logged in`);
   }
 );
