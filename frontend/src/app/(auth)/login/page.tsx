7a554a6d291a0bcad0eedb6c75b9a37ac79275e4
Glenn Chiang <glennchiang24@gmail.com>
2023-10-30 15:22:36 +0800

    Added login submit handler

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,31 +1,70 @@
+"use client";
+
+import { login } from "@/api/auth";
+import { ErrorMessage } from "@/components/ErrorMessage";
 import { SubmitButton } from "@/components/buttons";
 import { faLock, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormFields = {
+  username: string;
+  password: string;
+};
 
 export default function Login() {
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>();
+
+  const onSubmit: SubmitHandler<FormFields> = async (credentials) => {
+    setIsPending(true);
+    try {
+      await login(credentials);
+    } catch (error) {
+      setError((error as Error).message);
+    }
+  };
+
   return (
     <section className="flex flex-col items-center gap-10 w-full p-4">
       <h1 className="text-2xl">Welcome to Synapse</h1>
 
-      <form className="flex flex-col gap-8 w-full md:w-1/2">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-8 w-full md:w-1/2"
+      >
         <div className="flex flex-col gap-4 ">
           <div className="flex flex-col gap-2">
             <label className="flex gap-2 items-center text-slate-400">
               <FontAwesomeIcon icon={faUserCircle} />
               Username
             </label>
-            <input className="bg-slate-900" />
+            <input
+              {...register("username", { required: "Username is required" })}
+              className="bg-slate-900"
+            />
           </div>
           <div className="flex flex-col gap-2">
             <label className="flex gap-2 items-center text-slate-400">
               <FontAwesomeIcon icon={faLock} />
               Password
             </label>
-            <input className="bg-slate-900" />
+            <input
+              {...register("password", { required: "Password is required" })}
+              className="bg-slate-900"
+            />
           </div>
         </div>
-        <SubmitButton>Login</SubmitButton>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+        <SubmitButton isPending={isPending}>Login</SubmitButton>
       </form>
 
       <div>
